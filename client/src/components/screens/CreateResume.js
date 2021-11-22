270493d032093eb26a3f2d0950a9b8996189ca4e
aikan <aikansh2001yadav@gmail.com>
2021-11-22 15:46:05 +0530

    Added Create resume functionality

--- client/src/components/screens/CreateResume.js
+++ client/src/components/screens/CreateResume.js
@@ -1,8 +1,68 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import '../CSS/createResums.css'
+
 
 const CreateResume = () => {
     return (
-        <h1>CreateResume</h1>
+        <Container className="resume">
+            <Row>
+                <Col sm={12} md={6} lg={4}>
+                    <div className="card ">
+                        <div style={{ margin: "10px" }}>
+                            <h1 className="personal">Personal Information</h1>
+                            <input
+                                type="text"
+                                placeholder="Name*"
+                                required
+                            />
+                            <input
+                                type="email"
+                                placeholder="Email Address*"
+                                required
+                            />
+                            <input
+                                type="text"
+                                placeholder="Institute Name*"
+                                required
+                            />
+                            <input
+                                type="text"
+                                placeholder="Major Course Name*"
+                                required
+                            />
+                            <input
+                                type="text"
+                                placeholder="Minor Course Name"
+                            />
+                            <input
+                                type="text"
+                                placeholder="Graduation Year*"
+                                required
+                            />
+                            <input
+                                type="text"
+                                placeholder="Institue Location*"
+                                required
+                            />
+                            <input
+                                type="text"
+                                placeholder="CGPA or Percentage*"
+                                required
+                            />
+                            <input
+                                type="text"
+                                placeholder="Minor Course Name"
+                            />
+                            <input
+                                type="text"
+                                placeholder="Minor Course Name"
+                            />
+                        </div>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
